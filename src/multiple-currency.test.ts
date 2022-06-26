9d735c258dbc4d768ad405cde66758ce8861f083
ssabii <fhy1930@gmail.com>
2022-06-27 07:52:53 +0900

    2장 타락한 객체

--- src/multiple-currency.test.ts
+++ src/multiple-currency.test.ts
@@ -3,8 +3,10 @@ import { Dollar } from "./multiple-currency";
 describe("다중 화폐 예제", () => {
   test("multiplication", () => {
     const five = new Dollar(5);
+    let product = five.times(2);
+    expect(product.amount).toBe(10);
 
-    five.times(2);
-    expect(five.amount).toBe(10);
+    product = five.times(3);
+    expect(product.amount).toBe(15);
   });
 });
