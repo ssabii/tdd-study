a83ac6ce2294a7fa4bdfbc35ed316cd9d506472b
ssabi <fhy1930@gmail.com>
2022-07-04 12:17:18 +0900

    5장 솔직히 말하자면

--- src/multiple-currency.test.ts
+++ src/multiple-currency.test.ts
@@ -1,4 +1,4 @@
-import { Dollar } from "./multiple-currency";
+import { Dollar, Franc } from "./multiple-currency";
 
 describe("다중 화폐 예제", () => {
   test("multiplication", () => {
@@ -10,5 +10,11 @@ describe("다중 화폐 예제", () => {
   test("equality", () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBe(true);
     expect(new Dollar(5).equals(new Dollar(6))).toBe(false);
+  });
+
+  test("franc multiplication", () => {
+    const five = new Franc(5);
+    expect(five.times(2).equals(new Franc(10))).toBe(true);
+    expect(five.times(3).equals(new Franc(15))).toBe(true);
   });
 });
--- src/multiple-currency.ts
+++ src/multiple-currency.ts
@@ -14,3 +14,20 @@ export class Dollar {
     return this.amount === dollar.amount;
   }
 }
+
+export class Franc {
+  private amount: number;
+
+  constructor(amount: number) {
+    this.amount = amount;
+  }
+
+  public times(multiplier: number) {
+    return new Franc(this.amount * multiplier);
+  }
+
+  public equals(object: Object) {
+    const franc = object as Franc;
+    return this.amount === franc.amount;
+  }
+}
