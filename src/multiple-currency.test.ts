7ff2834edc63321b2e80fe7b5bd775864489aae3
ssabi <fhy1930@gmail.com>
2022-07-04 12:11:57 +0900

    4장 프라이버시

--- src/multiple-currency.test.ts
+++ src/multiple-currency.test.ts
@@ -3,11 +3,8 @@ import { Dollar } from "./multiple-currency";
 describe("다중 화폐 예제", () => {
   test("multiplication", () => {
     const five = new Dollar(5);
-    let product = five.times(2);
-    expect(product.amount).toBe(10);
-
-    product = five.times(3);
-    expect(product.amount).toBe(15);
+    expect(five.times(2).equals(new Dollar(10))).toBe(true);
+    expect(five.times(3).equals(new Dollar(15))).toBe(true);
   });
 
   test("equality", () => {
