1212538c678aaaf91f13c27dddd7e1dc57d14447
ssabi <fhy1930@gmail.com>
2022-07-07 00:56:41 +0900

    13장 진짜로 만들기 (중간저장)

--- src/multiple-currency.test.ts
+++ src/multiple-currency.test.ts
@@ -1,4 +1,4 @@
-import { Money, Expression, Bank } from "./multiple-currency";
+import { Bank, Money, Sum } from "./multiple-currency";
 
 describe("다중 화폐 예제", () => {
   test("multiplication", () => {
@@ -24,5 +24,26 @@ describe("다중 화폐 예제", () => {
     const bank = new Bank();
     const reduced = bank.reduce(sum, "USD");
     expect(reduced.equals(Money.dollar(10))).toBe(true);
+  });
+
+  test("plus returns sum", () => {
+    const five = Money.dollar(5);
+    const result = five.plus(five);
+    const sum = result as Sum;
+    expect(sum.augend).toEqual(five);
+    expect(sum.addend).toBe(five);
+  });
+
+  test("reduce sum", () => {
+    const sum = new Sum(Money.dollar(3), Money.dollar(4));
+    const bank = new Bank();
+    const result = bank.reduce(sum, "USD");
+    expect(result).toEqual(Money.dollar(7));
+  });
+
+  test("reduce money", () => {
+    const bank = new Bank();
+    const result = bank.reduce(Money.dollar(1), "USD");
+    expect(result).toEqual(Money.dollar(1));
   });
 });
--- src/multiple-currency.ts
+++ src/multiple-currency.ts
@@ -1,9 +1,9 @@
 export class Money implements Expression {
-  protected amount: number;
+  protected _amount: number;
   protected _currency: string;
 
   constructor(amount: number, currency: string) {
-    this.amount = amount;
+    this._amount = amount;
     this._currency = currency;
   }
 
@@ -15,33 +15,61 @@ export class Money implements Expression {
     return new Money(amount, "CHF");
   }
 
+  // javascript에서는 클래스 외부에서 protected에 접근할 수 없기 때문에 만들어줌
+  public get amount() {
+    return this._amount;
+  }
+
   public currency() {
     return this._currency;
   }
 
   public times(multiplier: number): Money {
-    return new Money(this.amount * multiplier, this._currency);
+    return new Money(this._amount * multiplier, this._currency);
   }
 
   public plus(addend: Money): Expression {
-    return new Money(this.amount + addend.amount, this._currency);
+    return new Sum(this, addend);
+  }
+
+  reduce(to: string): Money {
+    return null;
   }
 
   public equals(object: Object) {
     const money = object as Money;
-    return this.amount === money.amount && this.currency() === money.currency();
+    return (
+      this._amount === money._amount && this.currency() === money.currency()
+    );
   }
 
   // 이건 굳이 작성안해도 되지 않을까?
   public toString(): string {
-    return this.amount + "" + this._currency;
+    return this._amount + "" + this._currency;
   }
 }
 
-export interface Expression {}
+export interface Expression {
+  reduce(to: string): Money;
+}
 
 export class Bank {
-  public reduce(source: Expression, to: string) {
-    return Money.dollar(10);
+  public reduce(source: Expression, to: string): Money {
+    return source.reduce(to);
+  }
+}
+
+export class Sum implements Expression {
+  public augend: Money;
+  public addend: Money;
+
+  constructor(augend: Money, addend: Money) {
+    this.augend = augend;
+    this.addend = addend;
+  }
+
+  public reduce(to: string) {
+    const amount = this.augend.amount + this.addend.amount;
+    return new Money(amount, to);
   }
 }
