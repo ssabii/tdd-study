2cf9e408673f3c4c9a94d065f125d8d09850286e
ssabi <fhy1930@gmail.com>
2022-07-19 23:56:13 +0900

    14장 테스트 Money.equals로 수정

--- src/multiple-currency.test.ts
+++ src/multiple-currency.test.ts
@@ -30,27 +30,27 @@ describe("다중 화폐 예제", () => {
     const five = Money.dollar(5);
     const result = five.plus(five);
     const sum = result as Sum;
-    expect(sum.augend).toEqual(five);
-    expect(sum.addend).toBe(five);
+    expect(sum.augend.equals(five)).toBe(true);
+    expect(sum.addend.equals(five)).toBe(true);
   });
 
   test("reduce sum", () => {
     const sum = new Sum(Money.dollar(3), Money.dollar(4));
     const bank = new Bank();
     const result = bank.reduce(sum, "USD");
-    expect(result).toEqual(Money.dollar(7));
+    expect(result.equals(Money.dollar(7))).toBe(true);
   });
 
   test("reduce money", () => {
     const bank = new Bank();
     const result = bank.reduce(Money.dollar(1), "USD");
-    expect(result).toEqual(Money.dollar(1));
+    expect(result.equals(Money.dollar(1))).toBe(true);
   });
 
   test("reduce money different currency", () => {
     const bank = new Bank();
     bank.addRate("CHF", "USD", 2);
     const result: Money = bank.reduce(Money.franc(2), "USD");
-    expect(result).toEqual(Money.dollar(1));
+    expect(result.equals(Money.dollar(1))).toBe(true);
   });
 });
