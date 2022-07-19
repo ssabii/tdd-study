267580ed6192c81aa6a2934ae8a343509a545db7
ssabi <fhy1930@gmail.com>
2022-07-20 08:27:15 +0900

    16장 드디어, 추상화 (중간저장)

--- src/multiple-currency.test.ts
+++ src/multiple-currency.test.ts
@@ -64,4 +64,29 @@ describe("다중 화폐 예제", () => {
     const result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
     expect(result.equals(Money.dollar(10))).toBe(true);
   });
+
+  test("sum plus money", () => {
+    const fiveBucks = Money.dollar(5) as Expression;
+    const tenFrancs = Money.franc(10) as Expression;
+    const bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    const sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+    const result = bank.reduce(sum, "USD");
+    expect(result.equals(Money.dollar(15))).toBe(true);
+  });
+
+  test("sum times", () => {
+    const fiveBucks = Money.dollar(5) as Expression;
+    const tenFrancs = Money.franc(10) as Expression;
+    const bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    const sum = new Sum(fiveBucks, tenFrancs).times(2);
+    const result = bank.reduce(sum, "USD");
+    expect(result.equals(Money.dollar(20))).toBe(true);
+  });
+
+  test("plus same currency returns money", () => {
+    const sum = Money.dollar(1).plus(Money.dollar(1));
+    expect(sum instanceof Money).toBe(true);
+  });
 });
--- src/multiple-currency.ts
+++ src/multiple-currency.ts
@@ -29,7 +29,7 @@ export class Money implements Expression {
   }
 
   public plus(addend: Expression): Expression {
-    return new Sum(this, addend) as Expression;
+    return new Sum(this, addend);
   }
 
   public reduce(bank: Bank, to: string): Money {
@@ -53,6 +53,7 @@ export class Money implements Expression {
 export interface Expression {
   reduce(bank: Bank, to: string): Money;
   plus(addend: Expression): Expression;
+  times(multiplier: number): Expression;
 }
 
 export class Bank {
@@ -88,9 +89,15 @@ export class Sum implements Expression {
   }
 
   public plus(addend: Expression): Expression {
-    return null;
+    return new Sum(this, addend);
   }
 
+  public times(multiplier: number): Expression {
+    return new Sum(
+      this.augend.times(multiplier),
+      this.addend.times(multiplier),
+    );
+  }
 }
 
 class Pair {
